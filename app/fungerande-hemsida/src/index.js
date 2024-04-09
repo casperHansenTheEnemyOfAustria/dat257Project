568c416c53f583a7a368161368636fb15524d925
casper-hansen <60180409+casperHansenTheEnemyOfAustria@users.noreply.github.com>
2024-04-09 23:29:55 +0200

    !FIX! remade the website in regular react with added ts

--- app/fungerande-hemsida/src/index.js
+++ app/fungerande-hemsida/src/index.js
@@ -3,20 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { dbConnection } from './backend/dbConnection';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-let db = dbConnection.getInstance();
-
-console.log(db.getCountyByName('Västra Götaland'));
-
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
