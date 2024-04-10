1850fa695071d7c3ff73a9f6e12e56c6e326daa3
Huml4n <h.forsling@gmail.com>
2024-04-10 14:40:08 +0200

    <feat> dynamic dropbox

--- app/sverige-appen/src/app/page.tsx
+++ app/sverige-appen/src/app/page.tsx
@@ -5,6 +5,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import Dropdown from './frontend/dropdown';
+import LnNamn_Dropdown from './frontend/lnNamn_dropdown';
+import Resultbox from './frontend/result';
 
 
 
@@ -25,15 +27,18 @@ export default function Home() {
 
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
         <h1>Swedish Emission Status</h1> 
+
+
+        <Resultbox />
+
       </div>
 
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         
-          <Dropdown />
+          <LnNamn_Dropdown />
 
           <Dropdown />
 
-      
           <Dropdown />
 
         <a
