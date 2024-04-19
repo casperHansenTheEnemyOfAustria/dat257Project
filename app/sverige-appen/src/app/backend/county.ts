680d73a71cae432a4027a0c3827acd693c208ca0
casper-hansen <60180409+casperHansenTheEnemyOfAustria@users.noreply.github.com>
2024-04-19 10:54:49 +0200

    !FEAT! added class docs

--- app/sverige-appen/src/app/backend/county.ts
+++ app/sverige-appen/src/app/backend/county.ts
@@ -4,7 +4,20 @@ import { get } from "http";
 import { dbConnection } from "./dbConnection";
 import { Municipality } from "./minicipality";
 
-
+/**
+ * A class that represents a county that has a name,
+ *  a map of strings for info and a map of integers and lists of floats for emissions
+ * @class
+ * @classdesc A class representing a county
+ * @property {string} name - The name of the county
+ * @property {Map<string, string>} info - A map of strings for info
+ * @property {Map<number, number[]>} emissions - A map of integers and lists of floats for emissions
+ * @property {Promise<string[]>} municipalities - A promise of a list of the names of the municipalities in the county
+ * @property {dbConnection} db - A connection to the database
+ * @method getMunicipalityNames - A method that returns a list of the names of the municipalities in the county
+ * @method toJSON - A method that returns a json serializable representation of the county
+ * 
+ */
 export class County {
     private name: string;
     private info: Map<string, string>;
@@ -23,14 +36,20 @@ export class County {
     }
 
 
-    // returns the emissions map of the county
+    /**
+     * 
+     * @returns a list of the names of the municipalities in the county
+     */
     async getMunicipalityNames(): Promise<string[]> {
 
         return await this.municipalities;
     }
 
  
-
+    /**
+     * 
+     * @returns a json serializable representation of the county including all its elements
+     */
     async toJSON() {
         return {
             name: this.name,
