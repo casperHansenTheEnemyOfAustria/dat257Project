9d38a21fa7420b58f6bf1c2e97786a06a7206432
casper-hansen <60180409+casperHansenTheEnemyOfAustria@users.noreply.github.com>
2024-04-23 23:40:24 +0200

    made the map a client

--- app/sverige-appen/src/pages/frontend/map.js
+++ app/sverige-appen/src/pages/frontend/map.js
@@ -1,26 +1,35 @@
+'use client'
 import { MapContainer, TileLayer, GeoJSON } from "react-leaflet";
 import React from "react";
 import counties from "./public/geography/counties.json";
 import { useMapEvents } from 'react-leaflet/hooks'
 
-const Map = () => {
-  
+class Map extends React.Component {
+    onEachFeature(feature, layer) {
+        //bind click
+        layer.on({
+            click: function (e) {
+                console.log(feature.properties.name);
+            }
+        });
+
+
+        layer.bindPopup(feature.properties.name);
+
+
+    }
+    
+  render() {
   return (
     <div >
        <MapContainer center={[62.0, 15.0]} zoom={5} scrollWheelZoom={false} style={{ height: "50vh", width: "100vw" }} pmIgnore={false} >
-
-            <GeoJSON data={counties.features} onEachFeature={onEachFeature.bind(this)}/> 
-         
+            <GeoJSON data={counties} onEachFeature={this.onEachFeature} />
           </MapContainer>
     </div>
   );
+  }
 }
 
-function onEachFeature(feature, layer) {
-    //bind click
-
-    layer.bindPopup(feature.properties.name);
-}
 
 
 
