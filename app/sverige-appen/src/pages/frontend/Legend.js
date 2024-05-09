5fb51b5214a3050d5455a06dc0621e0b9900f619
casper-hansen <60180409+casperHansenTheEnemyOfAustria@users.noreply.github.com>
2024-05-09 10:27:12 +0200

    !FEAT! moved legend box

--- app/sverige-appen/src/pages/frontend/Legend.js
+++ app/sverige-appen/src/pages/frontend/Legend.js
@@ -4,16 +4,20 @@ import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 
 function Legend({ gradient }) {
-  gradient = gradient.gradient 
+  gradient = gradient.gradient.getColors() 
+  console.log(gradient)
+  var gstart = gradient[0]
+  var gend = gradient[-1]
+  console.log(gstart)
     return (
           <div className="legend">
               
-              <div className="legend-gradient" style={{background: `linear-gradient(to right, ${gradient})`}}>
+              <div className="legend-gradient" style={{background: `linear-gradient(to right, ${gstart}, ${gend}`}}>
 
                 <div className="legend-text">
                   
-                  <p>High</p>
-                  <p>Low</p>
+                  <p className="top">100%</p>
+                  <p className="bottom">0%</p>
                 </div>
               </div>
               
