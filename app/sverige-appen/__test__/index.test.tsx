c5714581daf4b1d1c7df2441dc6cdd9a5a1c16ed
casper-hansen <60180409+casperHansenTheEnemyOfAustria@users.noreply.github.com>
2024-05-15 16:02:43 +0200

    !FEAT! added more tests

--- app/sverige-appen/__test__/index.test.tsx
+++ app/sverige-appen/__test__/index.test.tsx
@@ -64,18 +64,7 @@ jest.mock('../src/app/backend/dbConnection', () => {
     };
   });
   
-//   describe('DbConnection', () => {
-//     it('getCounty returns correct data', async () => {
-//         var db = dbConnection.getInstance();
-
-//       const county = await db.getCounty('Test');
   
-//       expect(county.name).toBe('Test');
-//       expect(county.emissions.get(2000)).toEqual([100]);
-//       expect(county.emissions.get(2001)).toEqual([200]);
-//       // ... more assertions ...
-//     });
-//   });
 
 
   // test that a county object is json serializable
@@ -94,3 +83,121 @@ jest.mock('../src/app/backend/dbConnection', () => {
         // ... more assertions ...
         });
     });
+
+    // testing if countys are correctly fetched from the database
+    jest.mock('../src/app/backend/dbConnection', () => {
+        return {
+            dbConnection: {
+                getInstance: jest.fn(() => {
+                    return {
+                        getCounty: jest.fn(() => {
+                        return ['Test'];
+                        }),
+                        getMunicipalitiesInCounty: jest.fn(() => {
+                        return ['Test'];
+                        }),
+                        getMunicipality: jest.fn(() => {
+                        return {
+                            name: 'Test',
+                            emissions: new Map([
+                            [2000, [100]],
+                            [2001, [200]],
+                            ]),
+                        };
+                        })
+
+
+                    }
+                })
+            }
+        }
+    });
+
+        // testing if countys are correctly fetched from the database
+    describe('DbConnection', () => {
+        it('getCounty returns correct data', async () => {
+            var db = dbConnection.getInstance();
+            const county = await db.getCounty('Test');
+            expect(county).toEqual(['Test']);
+        });
+    });
+
+
+    // testing if the list of municipalities in a county is correctly fetched from the database
+    jest.mock('../src/app/backend/dbConnection', () => {
+        return {
+            dbConnection: {
+                getInstance: jest.fn(() => {
+                    return {
+                        getMunicipalitiesInCounty: jest.fn(() => {
+                        return ['Test'];
+                        }),
+                        getMunicipality: jest.fn(() => {
+                        return {
+                            name: 'Test',
+                            emissions: new Map([
+                            [2000, [100]],
+                            [2001, [200]],
+                            ]),
+                        };
+                        }),
+                        getCounty: jest.fn(() => {
+                        return ['Test'];
+                        })
+                    }
+                })
+            }}})
+
+    describe('DbConnection', () => {
+        it('getMunicipalitiesInCounty returns correct data', async () => {
+            var db = dbConnection.getInstance();
+            const municipalities = await db.getMunicipalitiesInCounty('Test');
+            expect(municipalities).toEqual(['Test']);
+        });
+    });
+
+jest.mock('../src/app/backend/dbConnection', () => {
+    return {
+        dbConnection: {
+            getInstance: jest.fn(() => {
+            return {
+                getMunicipalitiesInCounty: jest.fn(() => {
+                return ['Test'];
+                }),
+                getMunicipality: jest.fn(() => {
+                return {
+                    name: 'Test',
+                    emissions: new Map([
+                    [2000, [100]],
+                    [2001, [200]],
+                    ]),
+                };
+                }),
+                getCounty: jest.fn(() => {
+                return ['Test'];
+                }),
+                getMunicipalitiesInCounty: jest.fn(() => {
+                return ['Test'];
+                })
+            };
+            }),
+        },
+    };
+  });
+
+describe('getInstance', () => {
+  it('returns correct data', () => {
+    const instance = dbConnection.getInstance();
+
+    const municipalities = instance.getMunicipalitiesInCounty();
+    expect(municipalities).toEqual(['Test']);
+
+    const municipality = instance.getMunicipality();
+    expect(municipality.name).toBe('Test');
+    expect(municipality.emissions.get(2000)).toEqual([100]);
+    expect(municipality.emissions.get(2001)).toEqual([200]);
+
+    const county = instance.getCounty();
+    expect(county).toEqual(['Test']);
+  });
+});
