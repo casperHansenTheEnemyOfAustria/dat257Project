e373a1ac27479f7e91d7914ae886e6b2477a1286
casper-hansen <60180409+casperHansenTheEnemyOfAustria@users.noreply.github.com>
2024-05-15 11:52:41 +0200

    !FEAT! added another unit test

--- app/sverige-appen/__test__/index.test.tsx
+++ app/sverige-appen/__test__/index.test.tsx
@@ -36,3 +36,61 @@ jest.mock('../src/app/backend/dbConnection', () => {
       // ... more assertions ...
     });
   });
+
+// test that the information is correctly fetched from the database especially getting a county
+jest.mock('../src/app/backend/dbConnection', () => {
+    return {
+        dbConnection: {
+            getInstance: jest.fn(() => {
+            return {
+                getMunicipalitiesInCounty: jest.fn(() => {
+                return ['Test'];
+                } ),
+                getMunicipality: jest.fn(() => {
+                return {
+                    name: 'Test',
+                    emissions: new Map([
+                    [2000, [100]],
+                    [2001, [200]],
+                    ]),
+                };
+                })
+                
+
+       
+            };
+            }),
+        },
+    };
+  });
+  
+//   describe('DbConnection', () => {
+//     it('getCounty returns correct data', async () => {
+//         var db = dbConnection.getInstance();
+
+//       const county = await db.getCounty('Test');
+  
+//       expect(county.name).toBe('Test');
+//       expect(county.emissions.get(2000)).toEqual([100]);
+//       expect(county.emissions.get(2001)).toEqual([200]);
+//       // ... more assertions ...
+//     });
+//   });
+
+
+  // test that a county object is json serializable
+    describe('County', () => {
+        it('toJSON returns correct data', async () => {
+        const county = new County('Test', new Map([
+            [2000, [100]],
+            [2001, [200]],
+        ]));
+    
+        const json = await county.toJSON();
+    
+        expect(json.name).toBe('Test');
+        expect(json.emissions[2000]).toEqual([100]);
+        expect(json.emissions[2001]).toEqual([200]);
+        // ... more assertions ...
+        });
+    });
