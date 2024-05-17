cfa296cb31f3ac3f5e2011258ac57c07f75ed92e
casper-hansen <60180409+casperHansenTheEnemyOfAustria@users.noreply.github.com>
2024-05-17 09:39:29 +0200

    !FIX! added dynamic styling and scroll so it works on more screens

--- app/sverige-appen/src/pages/frontend/globalGoal.js
+++ app/sverige-appen/src/pages/frontend/globalGoal.js
@@ -20,15 +20,15 @@ export const GlobalGoal = () => {
                   Information about global goal
                 </div>
                 <div className='popup-text'>
-                <p className='popup-text-1'>The project aims to highlight the emissions of the different regions in Sweden, to get a more insightful understanding of how the different counties and municipalities affect our country’s emissions. This correlates to number 11, Make cities and human settlements inclusive, safe, resilient and sustainable, and number 13, Take urgent action to combat climate change and its impacts, of UN sustainability goals.</p>
+                  <p className='popup-text-1'>The project aims to highlight the emissions of the different regions in Sweden, to get a more insightful understanding of how the different counties and municipalities affect our country’s emissions. This correlates to number 11, Make cities and human settlements inclusive, safe, resilient and sustainable, and number 13, Take urgent action to combat climate change and its impacts, of UN sustainability goals.</p>
 
-                <p>The first aim of this project is aligned with reducing the environmental impact of cities in Sweden, this correlates specifically with 11.6 in UN sustainability goals. Moreover the goal states that we should provide more attention to the air quality. Since our product is designed to show users and inform them regarding air pollution connected to different parts of our country, this clearly aligns with the goal.</p>
+                  <p>The first aim of this project is aligned with reducing the environmental impact of cities in Sweden, this correlates specifically with 11.6 in UN sustainability goals. Moreover the goal states that we should provide more attention to the air quality. Since our product is designed to show users and inform them regarding air pollution connected to different parts of our country, this clearly aligns with the goal.</p>
 
-                <p>The second subtarget we are trying to fulfill with this project is 13.3, which states to increase knowledge and institutional capacity on climate change mitigation. Since our program shows different emissions in Sweden, we try to create awareness regarding the environment in each municipality. Later in the process, we want the program to be used as a basis in municipal political discussions to compare with other cities in Sweden. </p>
+                  <p>The second subtarget we are trying to fulfill with this project is 13.3, which states to increase knowledge and institutional capacity on climate change mitigation. Since our program shows different emissions in Sweden, we try to create awareness regarding the environment in each municipality. Later in the process, we want the program to be used as a basis in municipal political discussions to compare with other cities in Sweden. </p>
 
                 </div>
                 <div className="popup-image"> </div>
-                <div>
+                <div className="popup-close-parent">
                   <button onClick={() => close()} id='popup-close-button'>
                     X
                   </button>
